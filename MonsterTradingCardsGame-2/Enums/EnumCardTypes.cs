8d41670700e8d101d9d3f62c67042e785d8cefca
FireGhostPhino <Jackspaar@outlook.at>
2023-10-08 11:34:50 +0200

    CardTypes

---
+++ MonsterTradingCardsGame-2/Enums/EnumCardTypes.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonsterTradingCardsGame_2.Enums
+{
+    internal class EnumCardTypes
+    {
+         /*
+            Fire_
+            Water_
+            Regular_
+         */
+
+        enum CardTypes
+        {
+            Fire_Spell,
+            Water_Spell,
+            Regular_Spell,
+            Fire_Goblin,
+            Water_Goblin,
+            Regular_Goblin,
+            Fire_Troll,
+            Water_Troll,
+            Regular_Troll,
+            Fire_Ork,
+            Water_Ork,
+            Regular_Ork,
+            Fire_Elve,
+            Water_Elve,
+            Regular_Elve,
+            Dragon,
+            Knight,
+            Krake
+        }
+    }
+}
