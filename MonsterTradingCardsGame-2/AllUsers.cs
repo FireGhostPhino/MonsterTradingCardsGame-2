d88f6c3a1766eba66fa3b895a39c5ec1d7160283
FireGhostPhino <Jackspaar@outlook.at>
2023-10-08 12:04:49 +0200

    Users Classes

---
+++ MonsterTradingCardsGame-2/AllUsers.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonsterTradingCardsGame_2
+{
+    internal class AllUsers
+    {
+        public List<User> userList = null;
+
+        public void CreateList(User user)
+        {
+            userList = new List<User>();
+            userList.Add(user);
+        }
+
+        public void AddUser(User user)
+        {
+            userList.Add(user);
+        }
+
+        public bool IsNewUsername(string username)
+        {
+            for(int i = 0; i < userList.Count; i++)
+            {
+                if (userList[i].Username == username)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ MonsterTradingCardsGame-2/User.cs
@@ -0,0 +1,48 @@
+﻿using MonsterTradingCardsGame_2.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonsterTradingCardsGame_2
+{
+    internal class User
+    {
+        private string _username;
+        private string _password;
+        private string _coins;
+
+        public string Username
+        {
+            get { return _username; }
+            set { _username = value; }
+        }
+
+        public string Password
+        {
+            get { return _password; }
+            set { _password = value; }
+        }
+
+        public string Coins
+        {
+            get { return Coins; }
+            set { Coins = value; }
+        }
+
+        public bool AddUser(string username, string password, AllUsers userList)
+        {
+            if (userList.IsNewUsername(username))
+            {
+                Username = username;
+                Password = password;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
